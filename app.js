897a2fe305d9c592fdb4ba2b0561289fb0e60c35
shuradrozd@mail.ru <bonou2u2>
2018-12-28 18:25:05 +0300

    start work on the project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const path = require('path');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server run on the ${port}`);
+});
+
